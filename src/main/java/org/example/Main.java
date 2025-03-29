1a053653e30518fd751d302734e810fffefc7449
LRLayan <rameshlayan4@gmail.com>
2025-03-30 02:16:09 +0530

    Questions01()

---
+++ src/main/java/org/example/Main.java
@@ -0,0 +1,11 @@
+package org.example;
+
+import org.example.questions.Questions;
+
+public class Main {
+    public static void main(String[] args) {
+        Questions questions = new Questions();
+
+        questions.Questions01();
+    }
+}
