2fec7a972c14bb1b5c612950d0a11fa6e8b027ff
LRLayan <rameshlayan4@gmail.com>
2025-04-01 02:21:07 +0530

    FindDuplicateCount()

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,10 +1,12 @@
 package org.example;
 
+import org.example.questions.Arrays;
 import org.example.questions.Questions;
 
 public class Main {
     public static void main(String[] args) {
         Questions questions = new Questions();
+        Arrays arrays = new Arrays();
 
 //        questions.Questions01();
 //        questions.Questions02();
@@ -16,6 +18,8 @@ public class Main {
 //        questions.Questions08();
 //        questions.Questions09();
 //        questions.Questions10();
-        questions.Questions11();
+//        questions.Questions11();
+
+        arrays.FindDuplicateCount();
     }
 }
